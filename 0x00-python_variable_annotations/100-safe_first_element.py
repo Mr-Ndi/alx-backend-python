3a87a72a164de82dca49e9f064baa1b1ea1ead25
Mr-Ndi <onlythenotes@gmail.com>
2024-07-04 22:21:14 +0200

    Making a push or a change

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
-
 """
-    Annoting the function as asked
+This module provides a function `safely_get_value` that returns
+the value for a given key from a dictionary if the key exists,
+otherwise returns a default value.
 """
 
-from typing import Sequence, Any, Union
+from typing import Any, Mapping, TypeVar, Union
 
-NoneType = type(None)
+T = TypeVar('T')
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
-    """Documenting the asked function"""
-    if lst:
-        return lst[0]
+def safely_get_value(dct: Mapping,
+                     key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """
+    Returns the value for a given key from a dictionary
+    if the key exists, otherwise returns a default value.
+
+    Args:
+        dct: A dictionary from which to retrieve the value.
+        key: The key to look for in the dictionary.
+        default: The default value to return if the key is not found.
+
+    Returns:
+        The value associated with the key in the dictionary or the
+        default value if the key is not found.
+    """
+    if key in dct:
+        return dct[key]
     else:
-        return None
+        return default
