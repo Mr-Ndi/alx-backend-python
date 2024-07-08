71f801bcdb4500d9db62050745624b5cfee26f1d
Mr-Ndi <onlythenotes@gmail.com>
2024-07-08 14:49:35 +0200

    Making a push or a change

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
-
-"""
-    First module to first task
-"""
+''' this is the basic of asynch in python
+'''
 import asyncio
-import random
+from random import uniform
+'''
+this is the definition of the async function the delays random time'''
 
 
-async def wait_random(max_delay=10):
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+async def wait_random(max_delay: int = 10) -> float:
+
+    '''
+    we used the unifom method to get the random delay
+    and we specify the startig point (0) and the endig point (max_delay)
+    '''
+
+    new_delay = uniform(0, max_delay)
+
+    await asyncio.sleep(new_delay)
+    return new_delay
