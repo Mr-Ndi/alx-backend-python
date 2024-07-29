86de8f9b272f145c55dd8667be13138aef2d757c
Mr-Ndi <onlythenotes@gmail.com>
2024-07-29 19:03:04 +0200

    Correction of "1. Parameterize a unit test2"

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,8 +6,8 @@ from utils import access_nested_map
 
 """
     Module for class TestAccessNestedMap that
-     inherits from unittest.TestCase. And test
-     utils.access_nested_map
+    inherits from unittest.TestCase. And test
+    utils.access_nested_map.
 """
 
 
@@ -29,15 +29,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
     def test_access_nested_map_exception(self, nested_map, path):
         """
            Test that accessing a nested map with an invalid path
            raises KeyError.
-           Alspo it will check that the exception message is as expected
         """
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), path[-1])
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
 
 
 if __name__ == "__main__":
